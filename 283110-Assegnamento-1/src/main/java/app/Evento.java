857c4bd1b4ba72a3304e59719c0e3d1b062bc9c5
Gabriele <savastrecosmingabriele@gmail.com>
2019-10-05 18:26:22 +0200

    Added person removal from array of participants

--- 283110-Assegnamento-1/src/main/java/app/Evento.java
+++ 283110-Assegnamento-1/src/main/java/app/Evento.java
@@ -15,7 +15,17 @@ public class Evento {
 	this.iscritti = null;
   }
   
+  /*
+   * Aggiunge una persona all'interno della lista di iscritti dell'evento
+   * Nel caso la persona fosse già iscritta non verrà aggiunta
+   * Nel caso l'array di iscritti sia vuoto verrà creato un array
+   * di un elemento con quella persona al suo interno.
+   */
   public Persona[] AggiungiPersona(Persona persona) {
+	  
+	  if(iscritti == null) {
+		  iscritti = new Persona[0];
+	  }
 	  
 	  //Creo nuovo array maggiorato per il nuovo iscritto
 	  Persona[] nuovaLista = new Persona[iscritti.length + 1];
@@ -31,7 +41,33 @@ public class Evento {
 	  return iscritti;
   }
   
-  public Persona[] RimuoviPersona() {
+  /*
+   * Rimuove una persona dalla lista di iscritti dell'evento
+   * Nel caso l'evento non abbia iscritti oppure non esista tale persona nella lista di iscritti verrà ritornata la
+   * lista corrente di iscritti all'evento.
+   */
+  public Persona[] RimuoviPersona(Persona persona) {
+	  
+	  if(iscritti != null) {
+		  //Creo nuovo array ridotto per l'esclusione della persona rimossa
+		  Persona[] nuovaLista = new Persona[iscritti.length - 1];
+		  
+		  //Controllo che la persona sia già iscritta all'evento
+		  int indicePresenza = this.PresenzaIscritto(persona);
+		  
+		  if(indicePresenza == 0) {
+			  System.arraycopy(iscritti, 1, nuovaLista, 0, iscritti.length-1);
+			  iscritti = nuovaLista;
+		  }else if(indicePresenza > 0) {
+			  //Ricopio la prima parte dell'array
+			  System.arraycopy(iscritti, 0, nuovaLista, 0, indicePresenza);
+			  
+			  //Ricopio la seconda parte escludendo la persona rimossa
+			  System.arraycopy(iscritti, indicePresenza + 1, nuovaLista	, indicePresenza, iscritti.length - indicePresenza - 1);
+			  
+			  iscritti = nuovaLista;
+		  }
+	  }
 	  
 	  return iscritti;
   }
@@ -43,6 +79,7 @@ public class Evento {
 	  int indiceTrovato = -1;
 	  
 	  for(int i = 0; i < this.iscritti.length; i++) {
+		  //Overload della funzione equals() per classe di tipo Persona
 		  if(persona.equals(iscritti[i])) {
 			  indiceTrovato = i;
 		  }

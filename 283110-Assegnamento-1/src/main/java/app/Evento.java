df67b5be59f488512a5940e418db73db3d376ab0
Gabriele <savastrecosmingabriele@gmail.com>
2019-10-05 17:19:31 +0200

    Added subscription method and presence control to event

--- 283110-Assegnamento-1/src/main/java/app/Evento.java
+++ 283110-Assegnamento-1/src/main/java/app/Evento.java
@@ -14,4 +14,40 @@ public class Evento {
 	this.nome = nome;
 	this.iscritti = null;
   }
+  
+  public Persona[] AggiungiPersona(Persona persona) {
+	  
+	  //Creo nuovo array maggiorato per il nuovo iscritto
+	  Persona[] nuovaLista = new Persona[iscritti.length + 1];
+	  
+	  //Controllo che la persona non sia già iscritta all'evento
+	  if(this.PresenzaIscritto(persona) == -1) {
+		  System.arraycopy(iscritti, 0, nuovaLista, 0, iscritti.length);
+		  nuovaLista[iscritti.length] = persona;
+		  
+		  iscritti = nuovaLista;
+	  }
+	  
+	  return iscritti;
+  }
+  
+  public Persona[] RimuoviPersona() {
+	  
+	  return iscritti;
+  }
+  
+  /*Cerca l'indice in cui si trova una persona all'interno dell'array di iscritti
+   * Nel caso non sia trovata ritorna -1
+   */
+  public int PresenzaIscritto(Persona persona) {
+	  int indiceTrovato = -1;
+	  
+	  for(int i = 0; i < this.iscritti.length; i++) {
+		  if(persona.equals(iscritti[i])) {
+			  indiceTrovato = i;
+		  }
+	  }
+	  
+	  return indiceTrovato;
+  }
 }

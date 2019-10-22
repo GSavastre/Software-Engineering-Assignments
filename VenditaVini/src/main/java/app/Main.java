b422c3e034d7c8e20357975a71a8d774f153daa1
Gabriele <savastrecosmingabriele@gmail.com>
2019-10-22 21:58:27 +0200

    Aggiunta registrazione su file di nuovi utenti, etc...
    Aggiunti menu per cliente e impiegato
    Aggiunto controllo registrazione per utenti già esistenti

--- VenditaVini/src/main/java/app/Main.java
+++ VenditaVini/src/main/java/app/Main.java
@@ -1,12 +1,17 @@
 package app;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+//IMPORTANT TODO: Ciclare il menu, adesso una volta uscito dal menu oppure dopo un'eccezzione si usa System.exit(0) che forza l'utente a ripetere tutto il processo da capo
+//TODO: File loader
 public class Main {
 	
 	public static final String fileUtenti = "./src/main/resources/utenti.csv";
@@ -14,6 +19,8 @@ public class Main {
 	//Utente o impiegato che hanno eseguito l'accesso al sistema, una sorta di sessione (fatta male)
 	public static Utente utente = null;
 	public static Impiegato impiegato = null;
+	
+	public static Scanner input = new Scanner(System.in);
 
 	public static void main(String[] args) {
 		
@@ -53,7 +60,7 @@ public class Main {
 					System.out.println("Registrazione avvenuta con successo!");
 					ScelteUtente();
 				}else {
-					System.out.println("Registrazione fallita, le password non combaciano");
+					System.out.println("Registrazione fallita!");
 					System.exit(0);
 				}
 				break;
@@ -61,6 +68,8 @@ public class Main {
 			default:
 				System.out.println("Errore inaspettato!");
 		}
+		
+		input.close();
 	}
 	
 	
@@ -75,7 +84,7 @@ public class Main {
 	}
 	
 	private static int OttieniScelta(int limite) {
-		Scanner input = new Scanner(System.in);
+		//Scanner input = new Scanner(System.in);
 		
 		//Non 0 perché è il valore di default per l'uscita
 		int scelta = -1;
@@ -93,26 +102,47 @@ public class Main {
 			}
 		}while(scelta < 0 || scelta > limite);
 		
-		input.close();
+		//input.close();
 		
 		return scelta;
 	}
 	
 	private static void ScelteUtente() {
+		ArrayList<String> listaScelteUtente = new ArrayList<String>() {
+			{
+				add("Ricerca vini");
+				add("Acquista vini");
+			}
+		};
 		
+		StampaMenu(listaScelteUtente);
 	}
 	
 	private static void ScelteImpiegato() {
+		ArrayList<String> listaScelteImpiegato = new ArrayList<String>() {
+			{
+				add("Spedisci vini");
+				add("Rimpiazza vini");
+			}
+		};
 		
+		StampaMenu(listaScelteImpiegato);
 	}
 	
+	/*
+	 * Effettua il processo di login per un utente che sia cliente o impiegato
+	 * ritorna intero che identifica il ruolo dell'utente, inoltre istanzia un oggetto
+	 * utente o impiegato all'interno del main come specie di sessione a seconda del ruolo
+	 * che si ha dopo aver eseguito un login con successo
+	 * return 0 -> impiegato
+	 * return 1 -> cliente
+	 * return -1 -> errore
+	 */
 	private static int Login() {
 		
 		int ruolo = -1;
 		String riga = null;
 		String[] credenziali;
-		Scanner input = new Scanner(System.in);
-		
 		
 		System.out.print("Inserisci la email : ");
 		String email = input.nextLine();
@@ -134,8 +164,10 @@ public class Main {
 							//Verifico il ruolo
 							
 							if(credenziali[4].contains("0")) {
+								System.out.println("\nAccesso eseguito correttamente come impiegato!\n");
 								impiegato = new Impiegato(credenziali);
 							}else if(credenziali[4].contains("1")) {
+								System.out.println("\nAccesso eseguito correttamente come cliente!\n");
 								utente = new Utente(credenziali); 
 							}else {
 								return ruolo;
@@ -156,6 +188,8 @@ public class Main {
 			System.out.println(e.getMessage());
 			//e.printStackTrace();
 		}
+		
+		//input.close();
 		return ruolo;
 		
 	}
@@ -164,10 +198,8 @@ public class Main {
 	 * Inizia il processo di registrazione per un nuovo utente
 	 * return : Object Utente oppure null in caso di registrazione fallita (la ripetizione della password non è corretta)
 	 */
-	
-	//TODO: Scrivi su file utenti gli utenti nuovi
 	private static Utente RegistraUtente() {
-		Scanner input = new Scanner(System.in);
+		//Scanner input = new Scanner(System.in);
 		
 		System.out.print("Inserisci il nome : ");
 		String nome = input.nextLine();
@@ -184,9 +216,56 @@ public class Main {
 		System.out.print("Ripeti la password : ");
 		String pwdRipeti = input.nextLine();
 		
-		input.close();
+		String riga = null;
 		
-		return new Utente().Registrazione(nome, cognome, email, password, pwdRipeti);
+		//input.close();
+		
+		Utente nuovoUtente = new Utente().Registrazione(nome, cognome, email, password, pwdRipeti);
+		boolean userExists = false;
+		
+		//Ricerca email già esistente
+		try(BufferedReader fin = new BufferedReader(new FileReader(fileUtenti))){
+			//Legge tutte le righe del file
+			while((riga = fin.readLine())!= null) {
+				if(!riga.startsWith("#")) {
+					try {
+						String fileEmail = riga.split(",")[2];
+						if(email.equals(fileEmail)){
+							System.out.println("Questa email è già in uso, scegliere un altro indirizzo e riprovare");
+							userExists = true;
+						}
+					}catch(Exception e) {
+						e.printStackTrace();
+						return null;
+					}
+				}
+			}
+		}catch(FileNotFoundException e){
+			System.out.println("Impossibile trovare il file per l'accesso utenti.");
+			e.printStackTrace();
+		}
+		catch(IOException e){
+			System.out.println(e.getMessage());
+			//e.printStackTrace();
+		}
+		
+		if(!userExists) {
+			try(BufferedWriter fout = new BufferedWriter(new FileWriter(fileUtenti, true))){
+				
+				fout.append(System.lineSeparator()+nome+","+cognome+","+email+","+password+",1");
+				return nuovoUtente;
+				
+			}catch(FileNotFoundException e){
+				System.out.println("Impossibile registrare il nuovo utente");
+				e.printStackTrace();
+			}
+			catch(IOException e){
+				System.out.println(e.getMessage());
+				//e.printStackTrace();
+			}
+		}
+		
+		return null;
 	}
 
 }

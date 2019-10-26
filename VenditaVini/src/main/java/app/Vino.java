b335764aa7790b5b02635cf0b440cb87afb3ae02
Gabriele <savastrecosmingabriele@gmail.com>
2019-10-26 17:16:56 +0200

    Modifiche alla classe Vino
    Aggiunto costruttore per la ricerca di un vino e metodo per la stampa del vino

--- VenditaVini/src/main/java/app/Vino.java
+++ VenditaVini/src/main/java/app/Vino.java
@@ -5,17 +5,34 @@ public class Vino {
 	public String nome;
 	public int anno;
 	public String note;
-	public Vitigno vitigno;
+	//public Vitigno vitigno;
+	public String vitigno;
 	public int numeroBottiglie;
 	
 	public Vino() {}
 	
-	public Vino(String nome, int anno, String note,Vitigno vitigno, int numeroBottiglie){
+	public Vino(String nome, int anno, String note,String vitigno, int numeroBottiglie){
 		this.nome = nome;
 		this.anno = anno;
 		this.note = note;
 		this.vitigno = vitigno;
 		this.numeroBottiglie = numeroBottiglie;
+	}
+	
+	/*
+	 * Costruttore per la ricerca di un vino eseguita da un utente
+	 */
+	public Vino(String nome, int anno) {
+		this.nome = nome;
+		this.anno = anno;
+	}
+	
+	public void Print() {
+		System.out.println("Nome :"+ nome);
+		System.out.println("Anno :"+ anno);
+		System.out.println("Note :"+ note);
+		System.out.println("Vitigno :"+ vitigno);
+		System.out.println("Numero disponibile :"+ numeroBottiglie);
 	}
 
 }

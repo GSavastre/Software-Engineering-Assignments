50c6d6f23c26d84a8672a5aba95f082a161316e6
Gabriele <savastrecosmingabriele@gmail.com>
2019-11-07 12:27:35 +0100

    Client-Server Threading object stream testing
    Client

--- GestionaleAzienda/src/main/java/app/Client.java
+++ GestionaleAzienda/src/main/java/app/Client.java
@@ -1,9 +1,62 @@
 package app;
 
+import java.io.BufferedInputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.Random;
+
 public class Client {
+	
+	private static final int PORT = 5555;
+	private static final String HOST = "localhost";
+	
+	private static final int MAX = 100;
 
+	public void Run() {
+		try {
+			Socket client = new Socket(HOST, PORT);
+			
+			ObjectOutputStream os = new ObjectOutputStream(client.getOutputStream());
+			ObjectInputStream is = null;
+			
+			Random r = new Random();
+			
+			while(true) {
+				Request rq = new Request(r.nextInt(MAX));
+				
+				System.out.format("Client sends: %s to server", rq.GetValue());
+				
+				os.writeObject(rq);
+				os.flush();
+				
+				if(is == null) {
+					is = new ObjectInputStream(new BufferedInputStream(client.getInputStream()));
+				}
+				
+				Object o = is.readObject();
+				
+				if(o instanceof Response) {
+					Response rs = (Response) o;
+					
+					System.out.format(" and received: %s from Server%n", rs.GetValue());
+					
+					if(rs.GetValue() == 0) {
+						break;
+					}
+				}
+			}
+			
+			client.close();
+			
+		}catch(Exception e){
+			e.getMessage();
+			e.printStackTrace();
+		}
+	}
+	
 	public static void main(String[] args) {
-
+		new Client().Run();
 	}
 
 }

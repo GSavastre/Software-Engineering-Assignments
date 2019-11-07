59ccd9a4b27640cfeff7cd3e1d9224305cfbd990
Gabriele <savastrecosmingabriele@gmail.com>
2019-11-07 09:31:41 +0100

    Started working on employee
    TODO: Derived classes

--- GestionaleAzienda/src/main/java/personale/Impiegato.java
+++ GestionaleAzienda/src/main/java/personale/Impiegato.java
@@ -1,5 +1,28 @@
 package personale;
 
-public class Impiegato {
+import java.util.Date;
 
+import strutture.Sede;
+
+public class Impiegato {
+	public String nome;
+	public String cognome;
+	public String codiceFiscale;
+	public Sede sedeLavorativa;
+	public Date inizioAttivita;
+	public Date fineAttivita;
+	
+	public Impiegato(String nome, String cognome, String codiceFiscale, Sede sedeLavorativa, Date inizioAttivita, Date fineAttivita) {
+		this.nome = nome;
+		this.cognome = cognome;
+		this.codiceFiscale = codiceFiscale;
+		this.sedeLavorativa = sedeLavorativa;
+		this.inizioAttivita = inizioAttivita;
+		
+		if(fineAttivita.compareTo(inizioAttivita) > 0) {
+			this.fineAttivita = fineAttivita;
+		}else {
+			this.fineAttivita = null;
+		}
+	}
 }

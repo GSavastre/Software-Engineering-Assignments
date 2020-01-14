98dde5a25512e65f24aa04e43ffe35e14d81f010
Gabriele <savastrecosmingabriele@gmail.com>
2020-01-14 21:33:45 +0100

    Tried to get JavaFX to work

--- CircoloSportivoGUI/src/main/java/app/Main.java
+++ CircoloSportivoGUI/src/main/java/app/Main.java
@@ -3,11 +3,40 @@
 package app;
 
 import java.util.Scanner;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
 import java.util.Random;
 
-public class Main {
+public class Main extends Application{
+	
+	@Override
+	public void start(final Stage primaryStage) throws Exception {
+		Button btn = new Button();
+		
+		btn.setText("Say hello world!");
+		btn.setOnAction(e -> System.out.print("Hello, world!"));
+		
+		StackPane root = new StackPane();
+		root.getChildren().add(btn);
+		
+		Scene scene = new Scene(root, 300, 300);
+		
+		primaryStage.setTitle("Hello World!");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
 
 	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	
+	/*public static void main(String[] args) {
 		
 		Random random = new Random();
 		Admin nuovoAdmin = new Admin();
@@ -129,6 +158,8 @@ public class Main {
 		
 		input.close();
 	}
+	*/
+	
 	
 	
 	//Stampa i partecipanti ad un circolo

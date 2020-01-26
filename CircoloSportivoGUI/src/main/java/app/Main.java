bab506c108c4fcb89553cd2743d51389d65ea7c5
Gabriele <savastrecosmingabriele@gmail.com>
2020-01-26 18:35:25 +0100

    Removed unnecesary folders for lib

--- CircoloSportivoGUI/src/main/java/app/Main.java
+++ CircoloSportivoGUI/src/main/java/app/Main.java
@@ -7,16 +7,11 @@ import java.util.Scanner;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.util.Random;
-
 public class Main extends Application{
 	
 	private static Stage primaryStage;
